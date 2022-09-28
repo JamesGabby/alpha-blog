11fa44d914f03026b316902d49619059a60aa0ed
JamesGabby <jlghfe36@gmail.com>
2022-09-28 12:33:04 +0100

    add user signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :articles#, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :users, except: [:new]
   root "pages#home"
   get '/about', to: 'pages#about'
+  get '/signup', to: 'users#new'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_27_162618) do
+ActiveRecord::Schema.define(version: 2022_09_27_191959) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_09_27_162618) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
