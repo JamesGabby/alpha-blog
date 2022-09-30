b7f73250f87248b13070bf591c939969827e4128
JamesGabby <jlghfe36@gmail.com>
2022-09-30 14:56:08 +0100

    add user authentification

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in"
+      redirect_to user
+    else
+      flash.now[:alert] = "There was an error"
+      render 'new'
+    end
   end
 
   def destroy
