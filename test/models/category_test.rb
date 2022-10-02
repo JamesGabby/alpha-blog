0c427b5f71b181bd0bd385b5daef207a4b69397a
JamesGabby <jlghfe36@gmail.com>
2022-10-02 15:34:27 +0100

    set up unit test validations

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -5,4 +5,16 @@ class CategoryTest < ActiveSupport::TestCase
     @category = Category.new(name: "Sports")
     assert @category.valid?
   end
+
+  test "name should be present" do
+  end
+
+  test "name should be unique" do
+  end
+
+  test "name should not be too long" do
+  end
+
+  test "name should not be too short" do
+  end
 end
