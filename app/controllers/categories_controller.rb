91c2d3089e1b360d4623829cb2d84281e4e59baa
JamesGabby <jlghfe36@gmail.com>
2022-10-03 01:28:34 +0100

    add methods to category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,5 +6,22 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category created."
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
